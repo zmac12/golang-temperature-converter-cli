885879f1aafb685c568b02f22e752e34ddc3afcc
Zach McQuiston <zach.mcquiston35@gmail.com>
2021-07-15 21:02:40 -0600

    Adding work for last item

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
 var originUnit string
 var originValue float64
-
 var shouldConvertAgain string
 
 var err error
@@ -24,10 +23,21 @@ func main() {
 	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
-
-		if shouldConvertAgain != "Y" {
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
